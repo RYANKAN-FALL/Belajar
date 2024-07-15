2edcc73f74ebeb1a301d04140694f317c3fa7ec2
RYANKAN-FALL <prayogarkan12@gmail.com>
2024-07-15 12:55:22 +0700

    Test2

--- Tugas/Day 7/quiz.js
+++ Tugas/Day 7/quiz.js
@@ -2,10 +2,13 @@ const name = 'Prayoga';
 const bornYear = 2005;
 let hobby = 'Membaca';
 let address = 'Karimun';
+let hobby2 = 'Menonton';
 
 const currentYear = 2024;
 const age = currentYear - bornYear;
 
-const output = `Halo perkenalkan nama saya ${name}. Umur saya ${age} tahun. Hobi saya ${hobby}. Saya tinggal di ${address}.`;
+let gabunganHobby = `${hobby} dan ${hobby2}`;
+
+const output = `Halo perkenalkan nama saya ${name}. Umur saya ${age} tahun. Hobi saya ${gabunganHobby}. Saya tinggal di ${address}.`;
 
 console.log(output);
