fc28aaa44d259ecf8d66a25365277613d1ff4428
RYANKAN-FALL <prayogarkan12@gmail.com>
2024-07-15 18:06:28 +0700

    Test 9

--- Tugas/Day 7/quiz.js
+++ Tugas/Day 7/quiz.js
@@ -7,17 +7,17 @@ let hobby2 = 'Menonton Film';
 let addressCity = 'Karimun';
 let addressProvince = 'Kepulauan Riau';
 
-// Calculate age
+// Tahun Cihuy
 const currentYear = 2024;
 const age = currentYear - bornYear;
 
-// Combine hobbies
+// Hobi
 let combinedHobby = `${hobby1} dan ${hobby2}`;
 
-//address
+// Alamat
 const address = `${addressCity}, ${addressProvince}`
 
-// Output
+// Hasil
 const output = `Halo, perkenalkan nama saya ${name}, Nama lengkap saya ${fullName}. Umur saya ${age} tahun. Hobi saya ${combinedHobby}. Saya tinggal di ${address}.`;
 
 console.log(output);
